aeb390c5cdea095ac6b7bf5df0a1f562f8a2a128
Kateryna Tsyhaniuk <kyzkakatryska@gmail.com>
2024-10-17 00:20:05 +0300

    add all task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,28 @@
-// src/App.jsx
 import userData from "../userData.json";
+import friendList from "../friendList.json";
+import transactions from "../transactions.json";
 import Profile from "./Profile/Profile";
-
+import FriendList from "./FriendList/FriendList";
+import TransactionHistory from "./TransactionHistory/TransactionHistory";
+import "./App.css";
 export default function App() {
   return (
-    <div>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-    </div>
+    <>
+      <div>
+        <Profile
+          name={userData.username}
+          tag={userData.tag}
+          location={userData.location}
+          image={userData.avatar}
+          stats={userData.stats}
+        />
+      </div>
+      <div>
+        <FriendList friends={friendList} />
+      </div>
+      <div>
+        <TransactionHistory items={transactions} />
+      </div>
+    </>
   );
 }
